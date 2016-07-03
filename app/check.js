509bd1eadb7ad3993fb534beb60036c82481c771
rowen92 <maksym.pakhn@gmail.com>
2016-07-03 22:51:36 +0300

    doc

--- app/check.js
+++ app/check.js
@@ -1,9 +1,24 @@
 "use strict";
 
+/**
+ * Check module
+ * @module app/check
+ */
+
+/**
+ * Represents a check
+ * @constructor
+ * @param {array} stringArray String array
+ */
 function Check ( stringArray ) {
   this.stringArray = stringArray;
 }
 
+/**
+ * Unique array
+ * @method
+ * @return {array} Unique the elements of the array
+ */
 Array.prototype.unique = function() {
   var obj = {},
     arrayLength = this.length,
@@ -20,21 +35,32 @@ Array.prototype.unique = function() {
   return uniqueArray;
 };
 
+/**
+ * Check if the string in the first element of the array contains all of the letters of the string
+ * in the second element of the array
+ * @method
+ * @param  {integer} firstIndex  Index of the first element of the array
+ * @param  {integer} secondIndex Index of the second element of the array
+ * @return {boolean}             Result of the check
+ */
 Check.prototype.containsAllLetters = function( firstIndex, secondIndex ) {
   this.firstIndex = firstIndex;
   this.secondIndex = secondIndex;
+
   var firstString = this.stringArray[ firstIndex ].split("").unique(),
     secondString = this.stringArray[ secondIndex ].split("").unique();
+
   var firstStringLength = firstString.length,
     secondStringLength = secondString.length,
     i,
     j,
     k = 0;
 
-  for ( i = 0; i < secondStringLength; i++ ) {
-    for ( j = 0; j < firstStringLength; j++ ) {
-      if ( secondString[ i ] == firstString[ j ] ) {
+  for ( j = 0; j < secondStringLength; j++ ) {
+    for ( i = 0; i < firstStringLength; i++ ) {
+      if ( secondString[ j ] == firstString[ i ] ) {
         k += 1;
+        continue;
       }
     }
   }
--- app/index.js
+++ app/index.js
@@ -1,5 +1,5 @@
 "use strict";
 
 var Check = require("./check");
-var check = new Check([ "oen1d", "1one", "three" ]);
+var check = new Check([ "abcd", "bad", "three" ]);
 console.log( check.containsAllLetters( 0, 1 ) );
